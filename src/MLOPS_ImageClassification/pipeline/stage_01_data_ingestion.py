1fdc6234ea9376468c437e1dc18aabb831afc113
GattoMac <traceformula2000@gmail.com>
2023-09-17 07:11:36 -0700

    Implement Data Ingestion Pipeline

---
+++ src/MLOPS_ImageClassification/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,32 @@
+import sys
+import os
+sys.path.append(os.path.abspath('./'))
+from src.MLOPS_ImageClassification.config.configuration import ConfigurationManager
+from src.MLOPS_ImageClassification.components.data_ingestion import DataIngestion
+from src.MLOPS_ImageClassification import logger
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
