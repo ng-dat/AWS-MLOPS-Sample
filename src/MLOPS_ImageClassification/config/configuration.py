9c6997545ef3731c9bc0b362e3f8e9521e8f121e
GattoMac <traceformula2000@gmail.com>
2023-09-26 21:03:15 -0700

    Implement "Prepare Model" pipeline

--- src/MLOPS_ImageClassification/config/configuration.py
+++ src/MLOPS_ImageClassification/config/configuration.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from pathlib import Path
 sys.path.append(os.path.abspath('./'))
 from src.MLOPS_ImageClassification.constants import (
     CONFIG_FILE_PATH,
@@ -10,7 +11,8 @@ from src.MLOPS_ImageClassification.utils.common import (
     create_directories
 )
 from src.MLOPS_ImageClassification.config.config_entity import (
-    DataIngestionConfig
+    DataIngestionConfig,
+    PrepareBaseModelConfig
 )
 
 
@@ -34,3 +36,18 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir
         )
         return data_ingestion_config
+
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        create_directories([config.root_dir])
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+        return prepare_base_model_config
---
+++ src/MLOPS_ImageClassification/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,32 @@
+import sys
+import os
+sys.path.append(os.path.abspath('./'))
+from src.MLOPS_ImageClassification.config.configuration import ConfigurationManager
+from src.MLOPS_ImageClassification.components.prepare_base_model import PrepareBaseModel
+from src.MLOPS_ImageClassification import logger
+
+
+STAGE_NAME = "Prepare base model"
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
